841132deecffe3af3c28df3e039716892026f9e1
Stefano Gabryel <esse.gab2@gmail.com>
2019-04-26 15:02:13 +0200

    Adding documentation draft

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright 2011 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+Package warp provides an abstraction layer to read the ARP table.
+It supports linux, osx and windows.
+*/
+package warp
