b771c7f78e0efd63881f8b38ce6caa7934c6f637
Stefano Gabryel <esse.gab2@gmail.com>
2019-04-26 23:07:48 +0200

    Adding code to handle non-standard MAC address format under OSX

--- scan_darwin.go
+++ scan_darwin.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os/exec"
 	"regexp"
+	"strings"
 )
 
 var arpRegexp = regexp.MustCompile(`^[^\d\.]+([\d\.]+).+\s+([a-f0-9:]{11,17})\s+on\s+([^\s]+)\s+.+$`)
@@ -36,7 +37,7 @@ func entriesFromARP() ([]Entry, error) {
 			continue
 		}
 
-		entry, err := entryFromRawData(m[1], m[3], m[2])
+		entry, err := entryFromRawData(m[1], normalizeMac(m[2]), m[3])
 		if err != nil {
 			return nil, fmt.Errorf("darwin.entriesFromARP: failed to create entry: %s", err.Error())
 		}
@@ -46,4 +47,19 @@ func entriesFromARP() ([]Entry, error) {
 
 	return entries, nil
 
+}
+
+// normalizeMac adds missing leading zeroes - the ARP table in OSX can contain MAC addresses that golang
+// is unable to parse like: f1:ec:4e:20:f2:6 which is missing the leading zero for the last portion of the
+// address (:6 instead of :06), this method would translate the MAC to f1:ec:4e:20:f2:06
+func normalizeMac(rawMac string) string {
+	parts := strings.Split(rawMac, ":")
+
+	for i, part := range parts {
+		if len(part) < 2 {
+			parts[i] = "0" + part
+		}
+	}
+
+	return strings.Join(parts, ":")
 }
