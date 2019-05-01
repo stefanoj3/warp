e862cc4f202aa6ff56f92a4e729015db151225ea
Stefano Gabryel <esse.gab2@gmail.com>
2019-05-01 19:05:19 +0200

    Adding custom executor option

--- options.go
+++ options.go
@@ -1,6 +1,9 @@
 package warp
 
-import "os/exec"
+import (
+	"errors"
+	"os/exec"
+)
 
 // Option is the type used to represent additional configuration for the ARPScanner
 type Option func(*ARPScanner) error
@@ -20,6 +23,18 @@ func CustomCommandExecutorOption(cmd string, args ...string) Option {
 		s.arpCommandExecutor = func() ([]byte, error) {
 			return exec.Command(cmd, args...).CombinedOutput()
 		}
+		return nil
+	}
+}
+
+// CustomExecutorOption will use the provided function as executor when trying to perform a scan
+func CustomExecutorOption(fn func() ([]byte, error)) Option {
+	return func(s *ARPScanner) error {
+		if fn == nil {
+			return errors.New("warp.CustomExecutorOption: the function provided is nil")
+		}
+
+		s.arpCommandExecutor = fn
 		return nil
 	}
 }
--- scan_integration_test.go
+++ scan_integration_test.go
@@ -1,6 +1,8 @@
 package warp_test
 
 import (
+	"errors"
+	"strings"
 	"testing"
 	"time"
 
@@ -70,5 +72,46 @@ func TestARPScannerWithCustomExecutor(t *testing.T) {
 
 	if len(entries) != 0 {
 		t.Errorf("no entries expected with custom executor and invlaid output, got %d", len(entries))
+	}
+}
+
+func TestCustomExecutorOptionShouldReturnAnErrorWhenFunctionIsNil(t *testing.T) {
+	_, err := warp.NewARPScanner(
+		warp.CustomExecutorOption(nil),
+	)
+	if err == nil {
+		t.Fatal("an error is expected when nil is passed instead of a function to the custom executor")
+	}
+
+	if !strings.Contains(err.Error(), "CustomExecutorOption") {
+		t.Fatal("the error is expected to come from CustomExecutorOption")
+	}
+}
+
+func TestScanShouldReturnAnErrorWhenExecutorFails(t *testing.T) {
+	customError := errors.New("my_custom_error")
+
+	scanner, err := warp.NewARPScanner(
+		warp.CustomExecutorOption(
+			func() (bytes []byte, e error) {
+				return nil, customError
+			},
+		),
+	)
+	if err != nil {
+		t.Fatalf("no error expected when creating a new scanner, got %s", err.Error())
+	}
+
+	entries, err := scanner.Scan()
+	if err == nil {
+		t.Fatal("the error is expected to come from CustomExecutorOption")
+	}
+
+	if !strings.Contains(err.Error(), customError.Error()) {
+		t.Fatal("the error is expected to come from CustomExecutorOption")
+	}
+
+	if entries != nil {
+		t.Fatal("no entries are expected when an error is produced")
 	}
 }
